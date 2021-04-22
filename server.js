d9518090e1dcca9f97fdd6cc912c301df63391fe
HamzaQahoush <hamza.qah@gmail.com>
2021-04-22 18:02:12 +0300

    Portfolio

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const express=require('express')
+const server= express()
+const PORT=process.nextTick.PORT||3000
+
+
+server.use(express.static('./public'));
+
+// server.get('/' , (req,res,)=>{
+// res.send('Welcome To My Page')
+
+// })
+
+
+
+
+server.listen(PORT , ()=>{
+    console.log(`Listening to port ${PORT}`)
+})
